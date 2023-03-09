bff4b00a3333948e98faa781ac6db7861cfbb6d0
ana <anasuvi55@gmail.com>
2023-03-09 20:37:48 +0100

    prueba

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-/**
- * TIC TAC TOE
- */
+//Inicializamos el turno de cada jugador
+let turno = true;
 
-let jugador1 = 'X';
-let jugador2 = 'O';
+let tablero = document.getElementsByClassName("casilla")
 
-/**
- * [0, 1, 2]
- * [3, 4, 5]
- * [6, 7, 8]
- */
-// let tablero = ['','','','','','','','',''];
-let tablero = document.getElementsByClassName('casilla');
+let posicionX = []
+let posicionO = []
 
-/**
- * [3, 4, 5]
- * [0, 3, 6]
- * [1, 4, 7]
- * [2, 5, 8]
- * [0, 4, 8]
- * [2, 4, 6]
- */
 let combinacionGanadora = [
     [3, 4, 5],
     [0, 3, 6],
@@ -30,60 +15,58 @@ let combinacionGanadora = [
     [2, 4, 6]
 ];
 
-function GANAR_X(){
-    let actual = [];
-    // Recorrer las casillas para ver su contenido
-    for (let i = 0; i < tablero.length; i++){
-        if(tablero[i].innerHTML == 'X'){
-            actual.push(i);
-        }
-    }
-    /**
-     * Utilizar Array.include para comprobar si una de las combinaciones correctas
-     * esta incluida en mi array de actual
-     */
-    for(let i = 0; i < combinacionGanadora.length ; i++){
-        if(actual.includes(combinacionGanadora[i][0]) && actual.includes(combinacionGanadora[i][1]) && actual.includes(combinacionGanadora[i][2])){
-            alert('GANAN LAS X');
-        }
-    }
+for (let i = 0; i < tablero.length; i ++){
+    tablero[i].setAttribute('onclick', `pintarCasilla(${i})`)
 }
 
-function GANAR_O(){
-    let actual = [];
-    // Recorrer las casillas para ver su contenido
-    for (let i = 0; i < tablero.length; i++){
-        if(tablero[i].innerHTML == 'O'){
-            actual.push(i);
-        }
-    }
-    /**
-     * Utilizar Array.include para comprobar si una de las combinaciones correctas
-     * esta incluida en mi array de actual
-     */
-    for(let i = 0; i < combinacionGanadora.length ; i++){
-        if(actual.includes(combinacionGanadora[i][0]) && actual.includes(combinacionGanadora[i][1]) && actual.includes(combinacionGanadora[i][2])){
-            alert('GANAN LAS O');
-        }
-    }
-}
-
-/**
- * La funcion ponerFicha pide al usuario una posicion para insertar la ficha.
- * Si la posicion esta vacia, inserta y en caso contrario, vuelve a pedir la posicion
- */
-function ponerFicha(){
-    /**
-     * Utilizamos el prompt para recoger la casilla del usuario 
-     */
-    let c = prompt('Dime una casilla donde colocar la ficha');
-    console.log(c);
-    
-    if(tablero[parseInt(c)].innerHTML == ''){
-        tablero[parseInt(c)].innerHTML = 'X';
-        GANAR_X();
+function pintarCasilla(i){
+    if (turno){
+        tablero[i].textContent = 'X'
+        posicionX.push(i)
+        posicionX.sort()
+        if (posicionX.length >= 3){
+            comprobarGanador(posicionX)
+        } 
+        //comprobarGanador(posicionX)
     }else{
-        alert('Esta casilla esta ocupada');
-        ponerFicha();
+        tablero[i].textContent = 'O'
+        posicionO.push(i)
+        posicionO.sort()
+        if (posicionO.length >= 3){
+            comprobarGanador(posicionO)
+        } 
     }
+
+    tablero[i].removeAttribute('onclick')
+    turno = !turno
+}
+
+function comprobarGanador(posicion){
+    for(let i  = 0; i < combinacionGanadora.length; i++){
+        let contador = 0
+        for(let j  = 0; j < posicion.length; j++){
+            if (combinacionGanadora[i].includes(posicion[j])){
+                contador++
+            }
+        }
+        if(contador == 3){
+            if (turno){
+                detenerJuego()
+                alert('ha ganado X')
+                //crear una funcion que alerte quien ha ganado con la opcion de volver a jugar
+            }else if (!turno){
+                alert('ha ganado O')
+            }else{
+                alert('has empatado')
+            }
+
+            for (let k = 0; k < combinacionGanadora[i].length; k++){
+                tablero[combinacionGanadora[i][k]].style.backgroundColor = 'green'
+            }
+        }
+    }
+}
+
+function detenerJuego(){
+
 }
