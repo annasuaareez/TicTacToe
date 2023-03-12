616757660103386380d1365d51cebf83d899207c
ana <anasuvi55@gmail.com>
2023-03-12 18:58:45 +0100

    tic tac toe

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ let posicionO = []
 let contadorX = 0
 let contadorO = 0
 
+let tiempo = 30
+let segundos = document.getElementById('segundos')
+
 let combinacionGanadora = [
     [3, 4, 5],
     [0, 3, 6],
@@ -19,8 +22,16 @@ let combinacionGanadora = [
     [2, 4, 6]
 ];
 
+/*
+En la funcion pintar casilla inicializamos el tiempo que tiena cada jugados 
+Analizamos que si en una fila los 3 huecos estan pintados son del mismo jugador
+Y pasamos a la funcion de comprobar ganador
+*/
 function pintarCasilla(i){
     if (turno){
+        tiempo = 30
+        segundos.textContent = tiempo
+
         tablero[i].textContent = 'X'
         posicionX.push(i)
         posicionX.sort()
@@ -28,6 +39,9 @@ function pintarCasilla(i){
             comprobarGanador(posicionX)
         }
     }else{
+        tiempo = 30
+        segundos.textContent = tiempo
+
         tablero[i].textContent = 'O'
         posicionO.push(i)
         posicionO.sort()
@@ -40,10 +54,18 @@ function pintarCasilla(i){
     turno = !turno
 }
 
+/*añadimos el evento de onclick el cual al pulsar y soltar se pinta la casilla seleccionada*/
 for (let i = 0; i < tablero.length; i ++){
     tablero[i].setAttribute('onclick', `pintarCasilla(${i})`)
 }
 
+/*
+La funcion comprobar ganador como su nombre indica comprueba el ganador
+Analizamos las casillas de las filas con las posibles combinaciones 
+Si coinficen el contador aumenta y si llega hasta 3 analizamos si esta jugando las X o las O
+Cuando gana un jugador se le proporciona un mensaje de enhorabuena asi como hacemos que no se pueda seguir clickando
+A su vez paramos en tiempo y la puntuacion del jugador ganador aumenta
+*/
 function comprobarGanador(posicion){
     for(let i  = 0; i < combinacionGanadora.length; i++){
         let contador = 0
@@ -59,18 +81,19 @@ function comprobarGanador(posicion){
                 for (let i = 0; i < tablero.length; i ++){
                     tablero[i].removeAttribute('onclick')
                 }
+                //clearInterval para que se pare el tiempo
+                clearInterval(temporizador)
                 contadorX++
                 añadirPuntuacion()
             }else if (!turno){
+                document.getElementById('mensaje').classList.add('visible')
+                document.getElementById('mensaje').innerHTML = 'JUGADOR O HAS GANADO'
                 for (let i = 0; i < tablero.length; i ++){
-                    document.getElementById('mensaje').classList.add('visible')
-                    document.getElementById('mensaje').innerHTML = 'JUGADOR O HAS GANADO'
                     tablero[i].removeAttribute('onclick')
                 }
-                contadorO++
-                añadirPuntuacion()
             }
 
+            //La combinacion ganadora se pintara de verde
             for (let k = 0; k < combinacionGanadora[i].length; k++){
                 tablero[combinacionGanadora[i][k]].style.backgroundColor = 'green'
             }
@@ -78,6 +101,12 @@ function comprobarGanador(posicion){
     }
 }
 
+/*
+La funcion de volverAJugar hace su funcion en el boton para que los usuarios puedan repetir la partida
+En esta funcion hacemos desaparecer el mensaje de enhorabuena, asi como las casillas
+Reseteamos las posiciones y el turno del jugador, así como el tiempo
+Y volvemos a imprimir la ejecucion que nos permite poner las fichas
+*/
 function volverAJugar(){
     for (let i = 0; i < tablero.length; i++) {
         document.getElementById('mensaje').innerHTML = ''
@@ -89,12 +118,21 @@ function volverAJugar(){
     posicionO = []
     turno = true
 
+    tiempo = 30
+    segundos.textContent = tiempo
+    temporizador = setInterval(cuentaAtras, 1000)
+
     for (let i = 0; i < tablero.length; i ++){
         tablero[i].setAttribute('onclick', `pintarCasilla(${i})`)
     }
 
 }
 
+boton.addEventListener('click', volverAJugar)
+
+/*
+En esta fucnion se actualizan los contadores en funcion de si el jugador a ganado o no
+*/
 function añadirPuntuacion(){
     let contadorJugador1 = document.getElementById('jugador1')
     let contadorJugador2 = document.getElementById('jugador2')
@@ -103,5 +141,26 @@ function añadirPuntuacion(){
     contadorJugador2.textContent = contadorO
 }
 
-boton.addEventListener('click', volverAJugar)
+//inicializamos el tiempo 
+segundos.textContent = tiempo
+
+/*
+El tiempo empieza en 30 segundos y retrodece 1 segundo hasta llegar al -1, en donde se vuelve a iniciar el temporizador a 30
+*/
+function cuentaAtras(){
+    tiempo = --tiempo <= -1 ? 30 : tiempo
+
+    //Imprimimos el valor de los segundos que quedan
+    segundos.textContent = tiempo
+
+    //en el momento de que el tiempo llegue a 0 avisamos al jugador que le toca que ha perdido su turno
+    if (tiempo == 0){
+        document.getElementById('mensaje').innerHTML = 'HAS PERDIDO EL TURNO'
+        turno = !turno 
+    }else{
+        document.getElementById('mensaje').innerHTML = ''
+    }
+}
+
+let temporizador = setInterval(cuentaAtras, 1000)
 
